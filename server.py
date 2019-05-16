c799403af8050525510b6b73dc634f5bdf406b2d
qgee123 <qgee123@gmail.com>
2019-05-17 07:48:56 +0800

    add newline for telnet

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 addresses = []
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serv.bind(('0.0.0.0', 80))
+serv.bind(('0.0.0.0', 14399))
 serv.listen(5)
 commandlist=['add', 'mul', 'div', 'sub']
 while True:
@@ -15,13 +15,13 @@ while True:
         data = conn.recv(4096)
         x = data.split()
         if data == "who is there":
-            conn.send(str(addresses).encode('utf-8'))
+            conn.send(str(addresses)+"\n".encode('utf-8'))
         elif data == "geronyl":
-            conn.send("i love you Leah <3".encode('utf-8'))
+            conn.send("i love you Leah <3"+"\n".encode('utf-8'))
         elif not data:
             break
         elif x[0].lower() not in commandlist:
-            conn.send("ERR: Wrong command inputed, ex. add, sub, mul, div.")
+            conn.send("ERR: Wrong command inputed, ex. add, sub, mul, div.\n")
             break
         
         elif x[0].lower() == 'add':
@@ -29,40 +29,40 @@ while True:
                 ans = int(x[1]) + int(x[2])
                 print "client want to calculate: " + str(x[1]) + "+" + str(x[2])
                 print "Send: " + str(ans)
-                conn.send("The answer is: " + str(ans).encode('utf-8'))
+                conn.send("The answer is: " + str(ans)+"\n".encode('utf-8'))
             except:
-    			conn.send("ERR: The value of x and y should be int.".encode('utf-8'))
+    			conn.send("ERR: The value of x and y should be int.\n".encode('utf-8'))
 
         elif x[0].lower() == 'sub':
             try:
                 ans = int(x[1]) - int(x[2])
                 print "client want to calculate: " + str(x[1]) + "-" + str(x[2])
                 print "Send: " + str(ans)
-                conn.send("The answer is: " + str(ans).encode('utf-8'))
+                conn.send("The answer is: " + str(ans)+"\n".encode('utf-8'))
             except:
-                conn.send("ERR: The value of x and y should be int.".encode('utf-8'))
+                conn.send("ERR: The value of x and y should be int.\n".encode('utf-8'))
 
         elif x[0].lower() == 'mul':
             try:
                 ans = int(x[1]) * int(x[2])
                 print "client want to calculate: " + str(x[1]) + "*" + str(x[2])
                 print "Send: " + str(ans)
-                conn.send("The answer is: " + str(ans).encode('utf-8'))
+                conn.send("The answer is: " + str(ans)+"\n".encode('utf-8'))
             except:
-                conn.send("ERR: The value of x and y should be int.".encode('utf-8'))
+                conn.send("ERR: The value of x and y should be int.\n".encode('utf-8'))
 
 
         elif x[0].lower() == 'div':
             try:
                 if x[1] == 0:
-                    conn.send("ERR: Math Error.".encode('utf-8'))
+                    conn.send("ERR: Math Error.\n".encode('utf-8'))
                 else:
                     ans = float(x[1]) / float(x[2])
                     print "client want to calculate: " + str(x[1]) + "/" + str(x[2])
                     print "Send: " + str(ans)
-                    conn.send("The answer is: " + str(ans).encode('utf-8'))
+                    conn.send("The answer is: " + str(ans)+"\n".encode('utf-8'))
             except:
-                conn.send("ERR: The value of x and y should be int.".encode('utf-8'))
+                conn.send("ERR: The value of x and y should be int.\n".encode('utf-8'))
 
     conn.close()
     print 'client disconnected'
