e6ab0f211cf91cf673b729bfd0e8c486855c303f
qgee123 <qgee123@gmail.com>
2019-05-17 08:42:17 +0800

    fix client

--- client.py
+++ client.py
@@ -15,13 +15,15 @@ while True:
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect(('210.213.231.10', 14499))
 	from_server = client.recv(4096)
+	print (from_server.decode('utf-8'))
+	npt = raw_input("enter command: ")
+	client.send(npt.encode('utf-8'))
+	
 	if npt ==  'geronyl':
 		while True:
 			print (from_server.decode('utf-8'))
-	else:
-		print (from_server.decode('utf-8'))
-	if npt.lower() == "quit" or npt.lower() == "exit":
-		break
-	npt = raw_input("enter command: ")
-	client.send(npt.encode('utf-8'))
+
+		
+	# if npt.lower() == "quit" or npt.lower() == "exit":
+	# 	break
 	client.close()
