6d1a59260406cc0b8e6d1a0ff2d0c0e17b8bb92b
qgee123 <qgee123@gmail.com>
2019-05-17 09:00:13 +0800

    fix client

--- client.py
+++ client.py
@@ -9,19 +9,21 @@ print('''
 	>>quit or exit [to stop the process]
 
 ''')
-
-
 while True:
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect(('210.213.231.10', 44499))
-	npt = raw_input("enter command: ")
-	client.send(npt.encode('utf-8'))
 	from_server = client.recv(4096)
-	if npt ==  'geronyl':
-		while True:
+	print (from_server.decode('utf-8'))
+	while True:
+		
+		npt = raw_input("enter command: ")
+		client.send(npt.encode('utf-8'))
+		from_server = client.recv(4096)
+		if npt ==  'geronyl':
+			while True:
+				print (from_server.decode('utf-8'))
+		else:
 			print (from_server.decode('utf-8'))
-	else:
-		print (from_server.decode('utf-8'))
-	# if npt.lower() == "quit" or npt.lower() == "exit":
-	# 	break
+		# if npt.lower() == "quit" or npt.lower() == "exit":
+		# 	break
 	client.close()
