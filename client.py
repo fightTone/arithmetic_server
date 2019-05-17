5f9017e530224ff93275e1cb41e9558f8312153c
qgee123 <qgee123@gmail.com>
2019-05-17 09:07:31 +0800

    fix client

--- client.py
+++ client.py
@@ -10,6 +10,7 @@ print('''
 
 ''')
 while True:
+    stoper = False
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect(('210.213.231.10', 44499))
 	from_server = client.recv(4096)
@@ -26,4 +27,7 @@ while True:
 			print (from_server.decode('utf-8'))
 		if npt.lower() == "quit" or npt.lower() == "exit":
 			break
-	client.close()
+			stoper == True
+	if stoper == 'True':
+    	break
+		client.close()
